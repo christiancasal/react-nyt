ca1eb794b78d43d2a30ac89f4e3c736362bf32b1
ccasal <casal.christian@gmail.com>
2016-06-15 21:41:44 -0400

    skeleton

--- app/config/main_controller.js
+++ app/config/main_controller.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var Articles = require('../models/article_model.js')[0]
-var db = require('../models/article_model.js')[1];
+var Articles = require('./mongo.js')[0]
+var db = require('./mongo.js')[1];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'New York Times Search' });
+  res.send('./public/index.html');
 });
 
 //TODO routes to update the api go here?
