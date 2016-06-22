2732899b6f35d3e8148fe2c6e6fbd4c34e15a91f
ccasal <casal.christian@gmail.com>
2016-06-22 19:46:58 -0400

    overreacting

--- app/components/search/results/results.js
+++ app/components/search/results/results.js
@@ -1,29 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import helper from '../../../utils/helper.js';
 
 class Results extends React.Component{
-  constructor(props){
+  constructor(props) {
     super(props);
       this.state = {
-
+        data : this.props.data
       }
   }
-  render(){
-    return (
-        <div className="list-group-item col-md-12">
-          <h5 className="col-md-12">This will be the title/url</h5>
-          <h6 className="col-md-8">This will be the date published</h6>
-            <div className='btn-group col-md-4'>
-              <a className='pull-right'>
-              <button className="btn btn-default">Save</button>
-              <button className="btn btn-default">View Article</button>
-              </a>
-            </div>
-          {/*<div>and this is {this.props.data}</div>*/}
-        </div>
 
-    )
+  componentWillMount(){
+
+    this.addResults.bind(this);
+
+    this.setState({
+      data: this.props.data
+    });
+  }
+  //TODO create a render results fucntion to catch all the results from the api call, then pass it into the render function. map it over the state.data
+  renderHome(){
+
+  }
+
+  addResults(){
+    console.log('hello im in render results');
+    console.log(this);
+
+    let {data} = this.props
+
+    console.log(data);
+
+    if(data.length === 0){
+
+    }
+    // for (var i = 0; i < this.state.data.length; i++) {
+    //   console.log(this.state.data[i])
+    // }
+  }
+//make render function as small as possible
+
+  render(){
+      return (
+          <div className="list-group-item col-md-12">
+            <div className="col-md-12">
+              <h5 className="col-md-12">{console.log('this is results.js prop data')}{console.log(this.props.data)}
+              {console.log('this is results.js state data')}{console.log(this.state.data)}</h5>
+              <h6 className="col-md-8">This will be the ...</h6>
+                <div className='btn-group col-md-4'>
+                  <a className='pull-right'>
+                    <button className="btn btn-default">Save</button>
+                    <button className="btn btn-default">View Article</button>
+                  </a>
+                </div>
+            </div>
+            {console.log('add this')}
+            {this.addResults()}
+          </div>
+      )
   }
 }
 //props
--- app/components/search/search.js
+++ app/components/search/search.js
@@ -3,20 +3,60 @@ import ReactDOM from 'react-dom';
 import Results from './results/results.js';
 import helper from '../../utils/helper.js';
 
-console.log(helper);
-
 class Search extends React.Component {
   //the constructor will taake in props whos state will need to change
   constructor(props){
     super(props);
-      this.state = {
-      data: ""
-      //insert state attributes here
+
+    this.state = {
+      article_data: []
+    //insert state attributes here
     };
+
+
   }
+
+  componentWillMount() {
+
+    this.callHelper.bind(this);
+
+  }
+
+
+  callHelper(){
+    let formValues = {
+      search: document.getElementById('search').value,
+      start: document.getElementById('start').value,
+      end: document.getElementById('end').value
+    }
+
+    let apiArr = []
+
+    helper.performApiCall(formValues.search, formValues.start, formValues.end, (function cb(res){
+
+          console.log(res);
+
+          for(var i = 0; i < res.length; i++){
+            let apiObj = {
+              id: res[i]._id,
+              pub_date: res[i].pub_date,
+              url: res[i].web_url,
+              abs: res[i].headline.main
+            }
+            apiArr.push(apiObj)
+            console.log(apiArr);
+          }
+
+          this.setState({
+            article_data: apiArr
+          });
+    }).bind(this));
+
+  }
+
   render(){
     let { formValues } = this.props;
-    let { } = this.state;
+    let { article_data } = this.state;
     return (
       <div className="col-md-12">
         <div className="panel panel-primary">
@@ -41,34 +81,17 @@ class Search extends React.Component {
               </div>
 
               <div className="pull-right">
-                <button type="button" className="btn btn-danger btn-sm" onClick={this.formHandler.bind(this)}>
+                <button type="button" className="btn btn-danger btn-sm" onClick={this.callHelper.bind(this)}>
                   <h4>Submit</h4>
                 </button>
               </div>
             </form>
           </div>
-        </div> {/*//end of panel*/}
-        <Results data={this.state.data} />
+        </div>
+        <Results data={ article_data } />
       </div> // end of component container
-    );
+    )
   }//end of render
-  formHandler(){
-    let formValues = {
-      search: document.getElementById('search').value,
-      start: document.getElementById('start').value,
-      end: document.getElementById('end').value
-    }
-    let response = helper.performApiCall(formValues.search, formValues.start, formValues.end);
-    console.log(response);
-
-    this.setState({
-      data: [
-        formValues.search,
-        formValues.start,
-        formValues.end
-      ]
-    });
-  }
 }
 //props
 
